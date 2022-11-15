218e9b14422f18e2fb50c9c36bd5d5e37298c91a
alissa <acd18a@acu.edu>
2022-11-15 09:20:54 -0600

    Doing the things with D20, need audio and something else. Idk

--- Swift_D20_AD/ViewController.swift
+++ Swift_D20_AD/ViewController.swift
@@ -8,12 +8,37 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var diceImageView: UIImageView!
+    @IBOutlet var textLabel: UILabel!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func buttonGoPressed(){
+        rollDice()
+    }
+    
+    func rollDice(){
+        print("Kachicka kaching")
+        let rolledNumber = Int.random(in: 1...20)
+        
+        let imageName = "d\(rolledNumber)"
+        
+        diceImageView.image = UIImage(named: imageName)
+        
+        if(rolledNumber==1){
+            textLabel.text = "Loser"
+        } else if(rolledNumber==20){
+            textLabel.text = "Winner"
+        }else{
+            textLabel.text = String(rolledNumber)
+        }
+        
+    }
 
 }
 
