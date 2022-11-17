38f13eaecc07d161f51c890fab58e4342d78d923
alissa <acd18a@acu.edu>
2022-11-17 10:01:11 -0600

    Fixed constraints of dice and label, added shake, added sound

--- Swift_D20_AD/ViewController.swift
+++ Swift_D20_AD/ViewController.swift
@@ -6,11 +6,15 @@
 //
 
 import UIKit
+import AVKit
 
 class ViewController: UIViewController {
     
     @IBOutlet var diceImageView: UIImageView!
     @IBOutlet var textLabel: UILabel!
+    
+    var audioPlayer: AVAudioPlayer?
+    
     
 
     override func viewDidLoad() {
@@ -23,6 +27,20 @@ class ViewController: UIViewController {
     }
     
     func rollDice(){
+        //let soundDir: URL = Bundle.main.url(forResource: "diceroll", withExtension: "mp3")!
+        var soundDir = Bundle.main.path(forResource: "rolldice", ofType: "mp3")!
+        //let player = AVPlayer(url: soundDir)
+        var url = URL(fileURLWithPath: soundDir)
+        
+        
+        do{
+            audioPlayer = try AVAudioPlayer(contentsOf: url)
+            audioPlayer?.play()
+        }
+        catch{
+            print("Audio no worky")
+        }
+        
         print("Kachicka kaching")
         let rolledNumber = Int.random(in: 1...20)
         
@@ -32,12 +50,28 @@ class ViewController: UIViewController {
         
         if(rolledNumber==1){
             textLabel.text = "Loser"
+             soundDir = Bundle.main.path(forResource: "failwah", ofType: "mp3")!
+             url = URL(fileURLWithPath: soundDir)
         } else if(rolledNumber==20){
             textLabel.text = "Winner"
+             soundDir = Bundle.main.path(forResource: "zfanfare", ofType: "mp3")!
+             url = URL(fileURLWithPath: soundDir)
         }else{
             textLabel.text = String(rolledNumber)
         }
+        do{
+            audioPlayer = try AVAudioPlayer(contentsOf: url)
+            audioPlayer?.play()
+        }
+        catch{
+            print("Audio no worky")
+        }
         
+    }
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        if(motion == .motionShake){
+            rollDice()
+        }
     }
 
 }
